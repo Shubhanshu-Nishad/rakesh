f9b75f95bb0c43431fa806579cf86df9a22d1ba5
rakesh <prorakeshraut@gmail.com>
2018-01-29 06:45:16 -0800

    python basic

---
+++ day1.py
@@ -0,0 +1,41 @@
+# 1. To print in python
+print("rakesh")
+
+# 2. To add in python
+a=2
+b=3
+c=a+b
+print(c)
+
+#3.To subtract in python
+a=5
+b=2
+c=a-b
+print(c)
+
+#4. To divide in python
+z=10
+y=2
+x=z/y
+print(x)
+# In this division when we divide 10 by 2. the answer comes in (.0) which is known as (FLOAT.)
+
+#5. To multiply the number twice at a time.
+a=2
+b=4
+c=a**b
+print(c)
+
+#6. To print any name for more than one times
+a="rakesh"
+b=5
+c=a*b
+print(c)
+#The output of this input is: (rakeshrakeshrakeshrakeshrakesh).
+
+#7. To print the whole number
+a=11
+b=2
+c=a//b
+print(c)
+#The output must be (5.5) but we use the (//) so the output will come in whole number (5).
