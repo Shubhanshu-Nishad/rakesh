513885375ab3c52c07b1d21ff01552ee26282db3
rakesh <prorakeshraut@gmail.com>
2018-01-29 07:39:04 -0800

    this is addition

---
+++ addition.py
@@ -0,0 +1,6 @@
+name=input("Enter your name: ")
+print(name)
+first=int(input("Enter First number: "))
+second=int(input("Enter Second number: "))
+add=first+second
+print("The sum of the numbers is: ",add)
