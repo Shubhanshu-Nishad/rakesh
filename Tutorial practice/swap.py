1a6c5ce7ecf0618563eb622d75f522d4588f5487
rakesh <prorakeshraut@gmail.com>
2018-01-30 07:03:28 -0800

    this is swap

---
+++ Tutorial practice/swap.py
@@ -0,0 +1,8 @@
+rakesh="dai"
+kushal="vai"
+milan=0
+print("The rakesh is :",rakesh,"kushal is :",kushal)
+milan=rakesh
+rakesh=kushal
+kushal=milan
+print("The swap rakesh is:",rakesh,"The swap kushal is:",kushal)
