a1069e4f631ffa18f1d632c157e95a9a8b592642
rakesh <prorakeshraut@gmail.com>
2018-02-06 18:56:17 +0545

    changing_the_value_of_variable

---
+++ tutorialpractice/swap.py
@@ -0,0 +1,15 @@
+rakesh="dai"
+kushal="vai"
+milan=0
+print("The rakesh is :",rakesh,"kushal is :",kushal)
+milan=rakesh
+rakesh=kushal
+kushal=milan
+print("The swap rakesh is:",rakesh,"The swap kushal is:",kushal)
+
+
+abc=("I like to play guitar and i am huge fan of rakesh raut and he play very good guitar")
+xyz=(abc.find("play"))
+tuv=(abc.find("play",xyz+1))
+print(tuv)
+print(abc[62:])
