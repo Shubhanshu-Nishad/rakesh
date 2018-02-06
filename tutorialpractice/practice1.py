1c2dc71db42eafe944b1b5cbbb8ebcd9cf49595c
rakesh <prorakeshraut@gmail.com>
2018-02-06 18:53:46 +0545

    helps_to_find_position_character

---
+++ tutorialpractice/practice1.py
@@ -0,0 +1,19 @@
+speed_of_light =("299792458")
+print(speed_of_light)
+# meter per minute
+light =(speed_of_light*60)
+print(light)
+
+hours=(9)
+hours=(hours+1)
+hours=(hours*2)
+print(hours)
+
+#To print the particular letter...
+xyz=("computer")
+print(xyz[1:5])
+print(xyz.find("u"))  #This code helps to find the position of a character..
+
+abc=("i like to play guitar and i am huge fan of rakesh raut and he play very good guitar")# if we type the letter which is not available in paragraph. The answer will come (-1).
+y = (abc.find("g"))
+print(abc[y:])
