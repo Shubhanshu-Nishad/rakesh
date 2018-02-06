ca281de8ac93ff18dc0ab6c087312699fbc432a2
rakesh <prorakeshraut@gmail.com>
2018-02-06 18:48:06 +0545

    statement_of_if

---
+++ tutorialpractice/ifstatement.py
@@ -0,0 +1,17 @@
+#if condition:
+#	statement
+
+
+a=int(input("Enter your number: "))
+
+#a = int(a)
+
+if a<50:
+	print("The number you typed is less than 50")
+elif a==50:
+	print("The number you typed is equal to 50")
+	elif a>50;
+else:
+	print("The number you typed is greater than 50")
+
+	
