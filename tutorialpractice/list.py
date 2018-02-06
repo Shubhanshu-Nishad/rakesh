139a2a3f66c43a2ec223e5d8773e43a637da7d4e
rakesh <prorakeshraut@gmail.com>
2018-02-06 18:49:33 +0545

    list

---
+++ tutorialpractice/list.py
@@ -0,0 +1,31 @@
+name=["rakesh","milan","shyam","hari"]
+print(name[1])
+print(name[3])
+print(name[-1])
+
+#To add name append is used.
+name.append("radha")
+print(name)
+
+name.append("goma")
+print(name)
+#To remove name remove is used.
+
+name.remove("milan")
+print(name)
+
+age=[15,17,18,20,30]
+print(age)
+
+
+#name.extend(age)
+#print(name)
+
+print(name,age)
+
+
+name=[10,15,20,25,30]
+print(name)
+
+print(name[len(name)-1])
+
