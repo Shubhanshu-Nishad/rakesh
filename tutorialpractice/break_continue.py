936968bfb0621b7982d55753b841986d4cb16484
rakesh <prorakeshraut@gmail.com>
2018-02-08 21:46:24 +0545

    break-continue

---
+++ tutorialpractice/break_continue.py
@@ -0,0 +1,22 @@
+#break and continue
+i = 0
+while i<5:
+	print(i)
+	i=i+1
+	if i == 3:
+		continue 
+	print("I am printing")
+
+#This is practice number 5
+first = input("Input your First Name : ")
+last = input("Input your Last Name : ")
+print ("Hello  " + last +" "+  first)
+
+#This is practice number 6
+num = input("Input some comma seprated numbers : ")
+print('num : ',num)
+
+#This is practice number 7
+
+color = ["green","blue","yellow" ,"red"]
+print(color[0],color[-1])
