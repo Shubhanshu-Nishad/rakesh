e76e7ed88f2aa9ba63ea6fc990d186db55ca7e09
rakesh <prorakeshraut@gmail.com>
2018-02-22 21:18:37 +0545

    Here are the some actions of variables

---
+++ variables.py
@@ -0,0 +1,23 @@
+rent=1220
+gas=202.5
+grocires=305.6
+print("The house rent is: ",rent)
+total=rent+gas+grocires
+print("The total ruppes including rent,gas,grociries is: ",total)
+
+item1="rent"
+item2="gas"
+item3="grocires"
+print("My Expense items are: ",item1,item2,item3)
+
+birth = 2056
+current_year = 2074
+Age=(current_year-birth)
+print("My age is: ",Age)
+
+first = "Rakesh"
+middle = "kumar"
+last = "Raut"
+fullname=(first,middle,last)
+print("My name is: ",fullname)
+
