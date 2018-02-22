4700817b383a0b70de0377abe17af22d60fc5bc4
rakesh <prorakeshraut@gmail.com>
2018-02-22 21:26:47 +0545

    The corrected codes of variables

--- variables.py
+++ variables.py
@@ -10,14 +10,16 @@ item2="gas"
 item3="grocires"
 print("My Expense items are: ",item1,item2,item3)
 
+#To find the age by the help of bith year and current year.
 birth = 2056
 current_year = 2074
 Age=(current_year-birth)
 print("My age is: ",Age)
 
-first = "Rakesh"
-middle = "kumar"
+#To print the full name by the help of three variables value.
+first = "Rakesh "
+middle = "kumar "
 last = "Raut"
-fullname=(first,middle,last)
+fullname=first+middle+last
 print("My name is: ",fullname)
 
