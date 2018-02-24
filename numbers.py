7e0c2e8df343becbcd4d136472aedd9c70188ba7
rakesh <prorakeshraut@gmail.com>
2018-02-24 20:12:02 +0545

    the numbers is being operated

---
+++ numbers.py
@@ -0,0 +1,15 @@
+nyc_bal=188
+bal_pitt=247
+total_distance=nyc_bal+bal_pitt
+print(total_distance)
+#To find the time
+mph=65
+time=total_distance/mph
+print(time)
+round(time,2)
+
+num=10+2*3
+print(num)
+
+num2=(10+2)*3
+print(num2)
