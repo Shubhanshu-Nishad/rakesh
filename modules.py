f5defbda824c9404a0176935d3ec7dcb20fd096a
rakesh <prorakeshraut@gmail.com>
2018-02-24 20:09:37 +0545

    in modules the import function is used

---
+++ modules.py
@@ -0,0 +1,10 @@
+#To print the square root of the number.
+import math
+print(math.sqrt(16))
+
+print(math.pow(2,5))
+
+#To print the calander of the current month.
+import calendar
+cal = calendar.month(2018,2)
+print(cal)
